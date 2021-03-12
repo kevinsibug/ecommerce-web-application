66265464ea5d1714653bb68d0dfab55334b54ffd
Kevin Sibug <kevindaniel.sibug@gmail.com>
2021-03-12 19:10:35 +0800

    Edited styling - made the productts the same heights

--- src/components/Products/Product/styles.js
+++ src/components/Products/Product/styles.js
@@ -2,7 +2,8 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(() => ({ 
     root: {
-        maxWidth: '70%'
+        maxWidth: '70%',
+        height: '100%',
     },
     media: {
         height: 0,
