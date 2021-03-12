0b78c64ff6bcfefac4db7f2144f29ed88ebbbc22
Ysa Ramos <53614555+ysaramos@users.noreply.github.com>
2021-03-12 23:34:08 +0800

    Added Add to cart

--- src/App.js
+++ src/App.js
@@ -4,24 +4,36 @@ import { commerce } from './lib/commerce';
 
 const App = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
         
         setProducts(data);
     };
+    
+    const fetchCart = async () => {
+        setCart(await commerce.cart.retrieve())
+    }
+
+    const handleAddToCart = async (productId, quantity) => {
+        const item = await commerce.cart.add(productId, quantity);
+
+        setCart(item.cart);
+    }
 
     useEffect(() => {
         fetchProducts();
+        fetchCart();
     }, []);
 
-    console.log(products);
+    console.log(cart);
 
     return (
         <div>
             <Menubar />
             E-commerce Web Application
-            <Products products={ products } />
+            <Products products={ products } onAddToCart={handleAddToCart} />
         </div>
     )
 }
