ff0a9b3007671537deba021bdf0f7432359437c9
Ysa Ramos <53614555+ysaramos@users.noreply.github.com>
2021-03-18 11:34:43 +0800

    Added link to view cart page

--- src/components/Menubar/Menubar.jsx
+++ src/components/Menubar/Menubar.jsx
@@ -1,29 +1,31 @@
 import React from 'react'
-
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
-
 import { ShoppingCart } from '@material-ui/icons';
-
 import useStyles from './styles';
+import { Link, useLocation } from 'react-router-dom';
 
 const Menubar = ( { totalItems } ) => {
     const classes = useStyles();
+    
+    const location = useLocation();
 
     return (
         <div>
             <AppBar position="fixed" className={classes.appBar} color="inherit">
                 <Toolbar>
-                    <Typography>
+                    <Typography component={Link} to="/">
                         <img src='' alt="" height="30px" className={classes.image} /> 
                         Digital Artworks Online Store
                     </Typography>
                     <div className={classes.grow} />
-                    <div className={classes.button} />
-                        <IconButton aria-label="Show cart items" color="inherit">
+                    {location.pathname === '/' && ( 
+                    <div className={classes.button}>
+                        <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary"> 
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
+                    </div> )}
                 </Toolbar>
             </AppBar>
             
